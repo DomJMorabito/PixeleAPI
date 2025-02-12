ad27ebe4551ec7a1ba068776e9bbc307ca9f3ab2
DomJMorabito <domjmorabito@gmail.com>
2025-02-11 22:15:55 -0500

    Implementations: - Got rid of unnecessary logs for ips.

--- Check-Auth/utils/middleware/cors.js
+++ Check-Auth/utils/middleware/cors.js
@@ -11,6 +11,5 @@ export const corsMiddleware = (req, res, next) => {
     if (req.method === 'OPTIONS') {
         return res.status(200).end();
     }
-    console.log(`${req.method} ${req.path} - IP: ${req.ip}`);
     next();
 }
--- Check-Username-Availability/utils/middleware/cors.js
+++ Check-Username-Availability/utils/middleware/cors.js
@@ -10,6 +10,5 @@ export const corsMiddleware = (req, res, next) => {
     if (req.method === 'OPTIONS') {
         return res.status(200).end();
     }
-    console.log(`${req.method} ${req.path} - IP: ${req.ip}`);
     next();
 }
